0070e0844264f699f47392a0d5756034a74de2d5
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2024-08-15 17:47:36 -0300

    Add files via upload

---
+++ go-api/cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// Repository
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	// UseCase
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+	// Controllers
+	ProductController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+	server.POST("/product", ProductController.CreateProduct)
+	server.GET("/product/:productId", ProductController.GetProductById)
+
+	server.Run(":8000")
+}
